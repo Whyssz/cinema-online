da6f85ab83104cb46d1aa11824507fab6f479128
Shilov Dmitry <dragoniiix@mail.ru>
2023-02-19 15:32:04 +0300

    add a funciton for the movie

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,28 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { ActorModule } from './actor/actor.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { getMongodbConfig } from './config/mongo.config';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { GenreModule } from './genre/genre.module';
+import { getMongodbConfig } from './config/mongo.config';
 import { FileModule } from './file/file.module';
+import { GenreModule } from './genre/genre.module';
+import { UserModule } from './user/user.module';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypegooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: getMongodbConfig,
-    }),
-    AuthModule,
-    UserModule,
-    GenreModule,
-    FileModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		TypegooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongodbConfig,
+		}),
+		AuthModule,
+		UserModule,
+		GenreModule,
+		FileModule,
+		ActorModule,
+		MovieModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
---
+++ back/src/movie/movie.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { MovieController } from './movie.controller';
+import { MovieModel } from './movie.model';
+import { MovieService } from './movie.service';
+
+@Module({ 
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: MovieModel,
+			},
+		]),
+	],
+	controllers: [MovieController],
+	providers: [MovieService],
+})
+export class MovieModule {}
